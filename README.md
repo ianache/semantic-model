ab21b8cb1253ed92a2b9fe272146fac3843b713f
root <root@OSRM>
2024-11-17 08:54:58 +0000

    add: metricas basicas

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # semantic-model
 Modelo Semantico
+
+## Fact Table
+
+```sql
+create table analytic.fact_horasreportadas(
+  note_id int primary key,
+  project_id int, 
+  milestone_id int, 
+  regdate timestamp, 
+  reporter varchar(20), 
+  hours numeric, 
+  issue_id int);
+```
+
+## Dimensions
+
+```sql
+create table analytic.dim_people(
+  username varchar(20) primary key, 
+  fullname varchar(60),
+  role varchar(20), 
+  salary decimal(10,2)
+);
+``` 
---
+++ cube/model/cubes/fact_horasreportadas.js
@@ -0,0 +1,41 @@
+cube(`fact_horasreportadas`, {
+  sql_table: `analytic.fact_horasreportadas`,
+  
+  data_source: `default`,
+  
+  joins: {
+    dim_people: {
+	    sql: `${dim_people}.username = ${CUBE}.reporter`,
+	    relationship: `many_to_one`
+    }    
+  },
+  
+  dimensions: {
+    note_id: {
+      sql: `note_id`,
+      type: `number`,
+      primary_key: true
+    },
+    
+    reporter: {
+      sql: `reporter`,
+      type: `string`
+    },
+    
+    regdate: {
+      sql: `regdate`,
+      type: `time`
+    }
+  },
+  
+  measures: {
+    count: {
+      type: `count`
+    }
+  },
+  
+  pre_aggregations: {
+    // Pre-aggregation definitions go here.
+    // Learn more in the documentation: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  }
+});
